c7cb5ecf11efa190b3b7fa22a3848ddcd3b411cd
niawjunior <pasupol@dome.cloud>
2019-01-03 15:01:32 +0700

    Protecting the Admin Routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -20,6 +20,7 @@ import { MyOrdersComponent } from './my-orders/my-orders.component';
 import { AuthService } from './auth.service';
 import { AuthGuard } from './auth-guard.service';
 import { UserService } from './user.service';
+import { AdminAuthGuard } from './admin-auth-guard.service';
 
 @NgModule({
   declarations: [
@@ -59,14 +60,16 @@ import { UserService } from './user.service';
         path: 'order-success', component: OrderSuccessComponent, canActivate: [AuthGuard]
       }, {
         path: 'my/orders', component: MyOrdersComponent, canActivate: [AuthGuard]
+      },
+      // for Login & Admin
+      {
+        path: 'admin/products', component: AdminProductsComponent, canActivate: [AuthGuard, AdminAuthGuard]
       }, {
-        path: 'admin/products', component: AdminProductsComponent, canActivate: [AuthGuard]
-      }, {
-        path: 'admin/orders', component: AdminOrdersComponent, canActivate: [AuthGuard]
+        path: 'admin/orders', component: AdminOrdersComponent, canActivate: [AuthGuard, AdminAuthGuard]
       }
     ])
   ],
-  providers: [AuthService, AuthGuard, UserService],
+  providers: [AuthService, AuthGuard, UserService, AdminAuthGuard],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
